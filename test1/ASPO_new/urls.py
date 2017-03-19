e3e9b0bbe77f278e5fc106be85eff43e966b82c2
matic-podpadec <matic.podpadec@gmail.com>
2017-03-19 21:03:46 +0100

    Forward to Yahara

--- test1/ASPO_new/urls.py
+++ test1/ASPO_new/urls.py
@@ -1,8 +1,11 @@
 
 from django.conf.urls import url
-
 from . import views
+
+# for static files
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
