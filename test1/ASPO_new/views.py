54dd9fa5159cc0453e021a2425eb49e96e8a1be7
matic-podpadec <matic.podpadec@gmail.com>
2017-03-19 21:03:46 +0100

    Forward to Yahara

--- test1/ASPO_new/views.py
+++ test1/ASPO_new/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
 
 def index(request):
-    return render(request, "ASPO_new/index.html")
+    return render(request, "ASPO_new/index_original.html")
