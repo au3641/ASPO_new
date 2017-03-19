ab66c17e0a4fe968ab101a2e05337b46f655fd73
au3641 <au3641@student.uni-lj.si>
2017-03-20 00:39:33 +0100

    Footer works. Double quotes magic. Need foodz. Send pizza plz.

--- test1/ASPO_new/urls.py
+++ test1/ASPO_new/urls.py
@@ -9,4 +9,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'home', views.home, name='home'),
+    url(r'menu', views.menu, name='menu'),
+    url(r'footer', views.footer, name='footer'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
