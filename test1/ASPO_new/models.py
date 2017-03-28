45caa01c318e14c51365b4392837de8fdd9c2278
au3641 <au3641@student.uni-lj.si>
2017-03-29 01:10:08 +0200

    God help us all. SQL migrations galore.

--- test1/ASPO_new/models.py
+++ test1/ASPO_new/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Testis(models.Model):
+    velikost = models.IntegerField(default=0)
+
+class Questions(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    text = models.TextField()
+    sequence = models.IntegerField()
+    type = models.IntegerField()
+
+class Question_answers(models.Model):
+    answer_id = models.IntegerField(primary_key=True)
+    text = models.TextField()
+    flag = models.IntegerField()
+    order_nr = models.IntegerField()
+    question_id = models.IntegerField()
+
+class depend(models.Model):
+    answer_id = models.ForeignKey(Question_answers, on_delete=models.CASCADE)
--- test1/test1/settings.py
+++ test1/test1/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ASPO_new',
+    'ASPO_new.apps.AspoNewConfig',
 ]
 
 MIDDLEWARE = [
