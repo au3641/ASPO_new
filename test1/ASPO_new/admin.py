6c286ea79daf0ae8e27c96107656029bf4f5c0f5
polz <polz@aufbix.org>
2017-04-08 21:45:44 +0200

    Add models to admin interface, move type from answer to question and make it a choice field

--- test1/ASPO_new/admin.py
+++ test1/ASPO_new/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from ASPO_new.models import *
 
 # Register your models here.
+
+admin.site.register(Questionnaire)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Disable)
+admin.site.register(User)
+admin.site.register(AnswerWeight)
--- test1/ASPO_new/models.py
+++ test1/ASPO_new/models.py
@@ -8,31 +8,64 @@
 from __future__ import unicode_literals
 from django.db import models
 
+
+
 class Questionnaire(models.Model):
     name = models.TextField(unique=True)
     introText = models.TextField(blank=True, null=True)
 
 
 class Question(models.Model):
+    def  __str__( self ):
+        return "{0}".format(
+            self.text,
+        )
+
+    QUESTION_TYPES = (
+        ('radio', 'radio'),
+        ('checkbox', 'checkbox'),
+        ('button', 'button'),
+        ('date', 'date'),
+        ('email', 'email'),
+        ('number', 'number'),
+        ('range', 'range'),
+        ('time', 'time'),
+        ('url', 'url'),
+        ('text', 'text'),
+    )
     questionnaire = models.ForeignKey(Questionnaire)
     text = models.TextField(blank=True, null=True)
-    idGroup = models.IntegerField(blank=True, null=True)
+    order = models.IntegerField(blank=True, null=True)
+    type = models.CharField(
+        max_length = 16, 
+        choices=QUESTION_TYPES,
+        default='radio',
+    )
 
 
 class Answer(models.Model):
+    def  __str__( self ):
+        return "{0}".format(
+            self.text,
+        )
     question = models.ForeignKey(Question)
     text = models.TextField(blank=True, null=True)
-    type = models.TextField()
     order = models.IntegerField(blank=True, null=True)
 
 
 class Disable(models.Model):
+    def  __str__( self ):
+        return "{0} {1}".format(
+            self.question,
+            ", ".join(requiredAnswers)
+        )
     question = models.ForeignKey(Question)
     requiredAnswers = models.ManyToManyField(Answer)
 
 
 class User(models.Model):
     answeredWith = models.ManyToManyField(Answer)
+
 
 class AnswerWeight(models.Model):
     answer = models.ForeignKey(Answer)
