62ccd685cd4483c4cb629aa2478d46ef514385fc
au3641 <au3641@student.uni-lj.si>
2017-04-08 20:43:38 +0200

    Prepared urls, serializers and views for models.

--- test1/ASPO_new/views.py
+++ test1/ASPO_new/views.py
@@ -5,34 +5,46 @@ import re
 # from django.http import HttpResponse
 
 
+# Index/home page
 def index(request):
-    #print(str(request.path))
     return render(request, "ASPO_new/index.html")
-"""
-def home(request):
-    return render(request, "ASPO_new/home.html")
 
-def menu(request):
-    return render(request, "ASPO_new/menu.html")
-
-def footer(request):
-    return render(request, "ASPO_new/footer.html")
-"""
+# Regex handler for static pages
 def any(request):
     # single occurence regex ^([^/]*/[^/]*)/ (ALEN OP)
-    # alternative method is just do string split (meh)
+    # Filters app name from url path
     subUrl = re.sub( r'^([^/]*/[^/]*)/', "", str(request.path))
     return render(request, subUrl)
 
 
-# REST FROM HERE ON OUT
 
-"""
-from .models import QuestionsOld
-from .serializers import QuestionSerializer
+# REST HANDLERS FROM HERE ON OUT
+
+from .models import *
+from .serializers import *
 from rest_framework import viewsets
 
-class QuestionViewSet(viewsets.ModelViewSet):
-    queryset = QuestionsOld.objects.using('aspo_db').all()
+# Returns info about ASPO questionnaire
+class QuestionnaireASPO(viewsets.ModelViewSet):
+    queryset = Questionnaire.objects.using('aspo_new_db').filter(name__exact='ASPO')
+    serializer_class = QuestionnaireSerializer
+
+# Returns every question in ASPO questionnaire
+class QuestionSetASPO(viewsets.ModelViewSet):
+    queryset = Question.objects.using('aspo_new_db').filter(questionnaire__name='ASPO')
     serializer_class = QuestionSerializer
-"""
+
+# Returns all the posible answers for ASPO question set
+class AnswerSetForASPO(viewsets.ModelViewSet):
+    queryset = Answer.objects.using('aspo_new_db').filter(question__questionnaire__name='ASPO')
+    serializer_class = AnswerSerializer
+
+# Returns answer weights for ASPO question set
+class AnswerWeightForASPO(viewsets.ModelViewSet):
+    queryset = AnswerWeight.objects.using('aspo_new_db').filter(answer__question__questionnaire__name='ASPO')
+    serializer_class = AnswerWeightSerializer
+
+# Returns disables for ASPO question set
+class DisableForASPO(viewsets.ModelViewSet):
+    queryset = Disable.objects.using('aspo_new_db').filter(question__questionnaire__name='ASPO')
+    serializer_class = DisableSerializer
