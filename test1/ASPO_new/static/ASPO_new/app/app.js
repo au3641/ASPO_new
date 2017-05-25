6c291fd3dc319371654716693bad7c86457e4d91
polz <polz@aufbix.org>
2017-05-25 21:06:56 +0200

    Add/reorganize pages

--- test1/ASPO_new/static/ASPO_new/app/app.js
+++ test1/ASPO_new/static/ASPO_new/app/app.js
@@ -14,39 +14,39 @@
 		function($routeProvider) {
 		$routeProvider
 			.when('/', {
-				templateUrl : 'ASPO_new/home.html',
+				templateUrl : 'app/pages/home.html',
 				controller  : 'BlankCtrl'
 			})
 			.when('/vprasalnik', {
-				templateUrl : 'ASPO_new/survey.html',
+				templateUrl : 'app/pages/survey.html',
 				controller  : 'SurveyCtrl'
 			})
 			.when('/vprasalnik/uvod', {
-				templateUrl : 'ASPO_new/survey-intro.html',
+				templateUrl : 'app/pages/survey-intro.html',
 				controller  : 'BlankCtrl'
 			})
 			.when('/o-spolno-prenosljivih-okuzbah/:view?', {
-				templateUrl : 'ASPO_new/std-info.html',
+				templateUrl : 'app/pages/std-info.html',
 				controller  : 'StdInfoCtrl'
 			})
 			.when('/bolezenski-znaki/:view?', {
-				templateUrl : 'ASPO_new/symptoms.html',
+				templateUrl : 'app/pages/symptoms.html',
 				controller  : 'SymptomsCtrl'
 			})
 			.when('/pregled-pri-zdravniku', {
-				templateUrl : 'ASPO_new/medical-examination.html',
+				templateUrl : 'app/pages/medical-examination.html',
 				controller  : 'BlankCtrl'
 			})
 			.when('/zascita', {
-				templateUrl : 'ASPO_new/protection.html',
+				templateUrl : 'app/pages/protection.html',
 				controller  : 'BlankCtrl'
 			})
 			.when('/primeri/:view?', {
-				templateUrl : 'ASPO_new/examples.html',
+				templateUrl : 'app/pages/examples.html',
 				controller  : 'ExamplesCtrl'
 			})
 			.when('/o-nas', {
-				templateUrl : 'ASPO_new/about.html',
+				templateUrl : 'app/pages/about.html',
 				controller  : 'BlankCtrl'
 			})
 			.otherwise({ redirectTo: '/' });
