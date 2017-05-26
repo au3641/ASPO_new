98c4c6b6be7f818387b446dfd42749b21656a849
polz <polz@aufbix.org>
2017-05-26 08:46:31 +0200

    fixed settings example

--- ASPO_django/ASPO_django/settings.py
+++ ASPO_django/ASPO_django/settings.py
@@ -1,2 +1,8 @@
 from .settings_common import *
-from .settings_secret import *
+try:
+    from ASPO_django.settings_secret import *
+except Exception, e:
+    import traceback
+    traceback.print_exc()
+    print("Remember to copy settings_secret-example.py into settings_secret.py and change the secret key!")
+    exit(1)
--- ASPO_django/ASPO_django/settings_secret-example.py
+++ ASPO_django/ASPO_django/settings_secret-example.py
@@ -1,3 +1,7 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
