d42cfc46f67ea434d7291b0e2e5c47edb8972002
polz <polz@aufbix.org>
2017-05-26 08:58:37 +0200

    continue fixing the settings

--- ASPO_django/ASPO_django/settings.py
+++ ASPO_django/ASPO_django/settings.py
@@ -1,7 +1,7 @@
 from .settings_common import *
 try:
     from ASPO_django.settings_secret import *
-except Exception, e:
+except Exception:
     import traceback
     traceback.print_exc()
     print("Remember to copy settings_secret-example.py into settings_secret.py and change the secret key!")
